57d02fadb4786178c33ca605fa8f926797a21d1d
M4dDev <christian@crod.io>
2024-03-14 00:39:08 +0100

    update all exercises. Missing extra functionality for Tic Tac Toe game.

--- C-Basics/untitled2/ex42WritingFiles.c
+++ C-Basics/untitled2/ex42WritingFiles.c
@@ -3,9 +3,22 @@
 //
 #include <stdio.h>
 
-int maiun(){
+int main(){
 
-    FILE *pF = fopen("test.txt", "w");
+    FILE *pF = fopen("C:\\Users\\chris\\Desktop\\test.txt", "a");
+    // w = write
+    // a = append
 
+    fprintf(pF, "\nSpongebob Squarepants");
+
+    fclose(pF);
+
+/*
+    if(remove("test.txt") == 0){
+        printf("That file was deleted successfully!");
+    } else{
+        printf("That file was NOT deleted!");
+    }
+ */
     return 0;
 }
